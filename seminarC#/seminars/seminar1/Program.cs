ba6cd15a338341f7a4ed1d4ad64985bbc9beea26
guzvalieva <124667129+guzvalieva@users.noreply.github.com>
2024-08-30 00:52:20 +0500

    Add files via upload

---
+++ seminarC#/seminars/seminar1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Quad of {num} is {quad}");
+
+// Задача2
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int quad = num2 * num2;
+
+// if (quad == num1)
+// {
+//     Console.WriteLine("First number is quad of second");
+// }
+// else
+// {
+//     Console.WriteLine("First nomber is not quad of second");
+// }
+
+// Задача3
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current = num * (-1);
+// while (current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+
+// }
+
+// Задача4
+
+Console.WriteLine("Input three-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($"Last digit of {num} is {result}");
