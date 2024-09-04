5d0b1bf6850b4d5d50d8c0eb02a31bfc412067b6
guzvalieva <124667129+guzvalieva@users.noreply.github.com>
2024-09-04 20:10:06 +0500

    Program.cs

--- Program.cs
+++ Program.cs
@@ -26,3 +26,53 @@ if (n < 1)
   return;
 }
 Console.WriteLine(NaturalNumber(n, 1));
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
+
+int n = Prompt("Input N: ");
+int m = Prompt("Input M: ");
+
+Console.WriteLine(SumOfElements(n, m));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Input M: ");
+int n = Prompt("Input N: ");
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
