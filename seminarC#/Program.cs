e142b6a9838c23d4d0e7caa26cbeec3abbbe371d
guzvalieva <124667129+guzvalieva@users.noreply.github.com>
2024-09-04 20:06:19 +0500

    Add files via upload

---
+++ seminarC#/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     Console.WriteLine("Число первое больше второго");
+// }
+// else 
+// {
+//     Console.WriteLine("Число второе больше первого");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
